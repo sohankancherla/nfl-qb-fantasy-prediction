09bac306ac50ffbafbab6872a664ca973c19a3d2
sohankancherla <30853467+sohankancherla@users.noreply.github.com>
2023-11-12 14:26:03 -0800

    completed merging data

---
+++ data/combine_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+for year in range(2017, 2024):
+    df = pd.read_csv(f"madden/madden_ratings_{year}.csv")
+    fp = pd.read_csv(f"fantasy_pros/{year}.csv").drop(columns=["Pos", "Team", "Unnamed: 0", "#", "AVG", "TTL", "Year"]).rename(columns={"Player":"Name"})
+    df = pd.merge(df, fp, on="Name")
+    defense = pd.read_csv(f"schedule/{year}/matchups.csv").drop(columns=["Unnamed: 0"])
+
+    players = df[["Team", "Name", "Rating"]]
+    weeks = 18
+    if year >= 2021:
+        weeks = 19
+    for week in range(1, weeks):
+        week_col = str(week)
+        players = pd.merge(players, df[["Name", week_col]], on="Name")
+        home_away_col = f"Week_{week_col}_Home_Away"
+        opponent_col = f"Week_{week_col}_Opponent"
+        temp_df = defense[["Team", home_away_col, opponent_col]]
+        players = pd.merge(players, temp_df, on="Team")
+    
+    players.to_csv(f"combined/{year}.csv")
---
+++ data/combine_schedule.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+for year in range(2017, 2024):
+    df = pd.read_csv(f"madden/madden_ratings_{year}.csv")
+
+    defense = df["Team"].drop_duplicates().reset_index(drop=True)
+    defense = defense.to_frame()
+    ratings = pd.read_csv(f"defense/{year}.csv")
+    ratings.set_index("DEFENSE", inplace=True)
+
+    weeks = 18
+    if year >= 2021:
+        weeks = 19
+    for i in range(1, weeks):
+        defense[f'Week_{i}_Home_Away'] = ''
+        defense[f'Week_{i}_Opponent'] = ''
+    for index, row in defense.iterrows():
+        team = row["Team"]
+        if team == "Redskins":
+            team = "Washington"
+        elif team == "Commanders":
+            team = "Washington"
+        elif team == "Football Team":
+            team = "Washington"
+        for i in range(1, weeks):
+            schedule = pd.read_csv(f"schedule/{year}/week_{i}.csv")
+            home_games = schedule[schedule['Home Team'] == team]
+            away_games = schedule[schedule['Away Team'] == team]
+            if not home_games.empty:
+                opponent = home_games['Away Team'].iloc[0]
+                defense.at[index, f'Week_{i}_Home_Away'] = 'Home'
+                rating = ratings.loc[opponent, 'DEF RATING']
+                defense.at[index, f'Week_{i}_Opponent'] = rating
+            elif not away_games.empty:
+                opponent = away_games['Home Team'].iloc[0]
+                defense.at[index, f'Week_{i}_Home_Away'] = 'Away'
+                rating = ratings.loc[opponent, 'DEF RATING']
+                defense.at[index, f'Week_{i}_Opponent'] = rating
+            else:
+                defense.at[index, f'Week_{i}_Home_Away'] = 'Bye'
+                defense.at[index, f'Week_{i}_Opponent'] = 'Bye'
+    
+    defense.to_csv(f"schedule/{year}/matchups.csv")
